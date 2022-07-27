cb2e5d4e8001435180996ef471911cb8ab9b43dc
s0alken <ropedent@gmail.com>
2022-07-26 21:29:26 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import Card from "./components/Card";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+  const [slip, setSlip] = useState(null);
+
+  useEffect(() => {
+    fetchSlip();
+  }, []);
+
+  const fetchSlip = async () => {
+    setSlip(null);
+
+    const response = await axios.get('https://api.adviceslip.com/advice');
+
+    setSlip(response.data.slip);
+
+  }
+
+  return <Card slip={slip} fetchSlip={fetchSlip} />;
+
 }
 
 export default App;
---
+++ src/components/Card.js
@@ -0,0 +1,28 @@
+import pattern_divider_desktop from '../images/pattern-divider-desktop.svg';
+import pattern_divider_mobile from '../images/pattern-divider-mobile.svg';
+import icon_dice from '../images/icon-dice.svg';
+
+const Card = ({ slip, fetchSlip }) => {
+
+    return (
+        <div className="card">
+            {slip ? (
+                <>
+                    <h1 className="card__heading">{`Advice #${slip.id}`}</h1>
+                    <p className="card__quote">{`“${slip.advice}”`}</p>
+                </>
+            ) : <p className="card__quote">Loading...</p>}
+
+            <picture className="card__divider">
+                <source srcset={pattern_divider_mobile} media="(max-width: 25em)" />
+                <img src={pattern_divider_desktop} alt="Divider" />
+            </picture>
+
+            <button className="card__button" onClick={fetchSlip}>
+                <img src={icon_dice} alt="Icon dice" />
+            </button>
+        </div>
+    )
+}
+
+export default Card;
